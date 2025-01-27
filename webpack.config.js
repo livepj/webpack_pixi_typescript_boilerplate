c77a20437f4e4653e0a890c466a7819ea187ab41
Oleksii Shpilov <chrismasroom@gmail.com>
2025-01-27 21:26:46 +0200

    update eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-/*eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
